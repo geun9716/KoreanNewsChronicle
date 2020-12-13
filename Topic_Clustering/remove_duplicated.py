f6ed6aa6c13573672c1dbce0e9bfd99b6aea5676
tkdxor97 <ghdtkdxor@naver.com>
2020-12-14 05:53:29 +0900

    remove_duplicated2

--- Topic_Clustering/remove_duplicated.py
+++ Topic_Clustering/remove_duplicated.py
@@ -3,14 +3,31 @@ import pandas as pd
 
 df=pd.read_csv('C:/Users/ghdtk/OneDrive/Desktop/VSCode/Python/Korean_News_Chronicle/KoreanNewsChronicle/Topic_Abstract/Data/201901.csv',encoding='utf-8')
 
-print(len(df))
+print('원래 기사 수 : ' + str(len(df)))
 
 dp = df.duplicated(['headline'], keep='first')
 count = 0
-for b in dp:
+for b in dp:            #중복된 기사 제거
     if(b == True):
         df.drop(df.index[count], inplace = True)
         count -= 1
     count += 1
 
-print(len(df))
+print('중복된 기사 제거후 기사수 : ' + str(len(df)))
+
+df = df.dropna()        #토픽이 없는 기사 제거
+
+print('토픽이 없는 기사 제거후 기사수 : ' + str(len(df)))
+
+count = 0
+for index, row in df.iterrows():        #토픽이 한개인 기사 제거
+    if(len(row['topics'].split(', ')) == 1):
+        df.drop(df.index[count], inplace = True)
+        count -= 1
+    count += 1
+
+print('토픽이 한개밖에 없는 기사 제거후 기사수 : ' + str(len(df)))
+
+stopword = ['A씨', '만', '억', '조']
+
+# topic에서 stopword 제거도 추가 예정
