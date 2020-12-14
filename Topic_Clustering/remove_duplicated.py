615fa02be24cf54a849f5722b970c1f20475b55d
geun9716 <crintop@gmail.com>
2020-12-14 18:32:07 +0900

    클러스터 전처리

--- Topic_Clustering/remove_duplicated.py
+++ Topic_Clustering/remove_duplicated.py
@@ -1,10 +1,20 @@
 import csv
 import pandas as pd
 
-df=pd.read_csv('C:/Users/ghdtk/OneDrive/Desktop/VSCode/Python/Korean_News_Chronicle/KoreanNewsChronicle/Topic_Abstract/Data/201905.csv',encoding='utf-8')
+filename = '201912.csv'
 
-print('원래 기사 수 : ' + str(len(df)))
-
+df=pd.read_csv(filename,encoding='utf-8')
+print('원래 기사 수 : ',len(df))
+df = df.dropna()        #토픽이 없는 기사 제거
+print('토픽이 없는 기사 제거후 기사수 : ',len(df))
+stopword = ['A씨', '만', '억', '조', '차', '그', '시', '대', '위', '분', '측', '곳']
+for index, row in df.iterrows():
+    topic = row['topics'].split(', ')
+    for str in topic:
+        if(str in stopword):
+            topic.remove(str)
+    topic_str = ", ".join(topic)
+    df.loc[index, 'topics'] = topic_str
 dp = df.duplicated(['headline'], keep='first')
 count = 0
 for b in dp:            #중복된 기사 제거
@@ -12,29 +22,13 @@ for b in dp:            #중복된 기사 제거
         df.drop(df.index[count], inplace = True)
         count -= 1
     count += 1
-
-print('중복된 기사 제거후 기사수 : ' + str(len(df)))
-
-df = df.dropna()        #토픽이 없는 기사 제거
-
-print('토픽이 없는 기사 제거후 기사수 : ' + str(len(df)))
-
-count = 0
-for index, row in df.iterrows():        #토픽이 한개인 기사 제거
-    if(len(row['topics'].split(', ')) == 1):
-        df.drop(df.index[count], inplace = True)
-        count -= 1
-    count += 1
-
-print('토픽이 한개밖에 없는 기사 제거후 기사수 : ' + str(len(df)))
-
-stopword = ['A씨', '만', '억', '조', '차', '그', '시', '대', '손', '양', '위', '분', '성', '측', '안', '곳']
-
+print('중복된 기사 제거후 기사수 : ',len(df))
+tmp_index=[]
 for index, row in df.iterrows():
-    topic = row['topics'].split(', ')
-    for str in topic:
-        if(str in stopword):
-            topic.remove(str)
-
-    topic_str = ", ".join(topic)
-    df.loc[index, 'topics'] = topic_str
+    tmp = row['topics'].split(',')
+    if len(tmp) < 4:
+        tmp_index.append(index)
+df = df.drop(tmp_index)
+print('토픽이 3개 이하 밖에 없는 기사 제거후 기사수 : ',len(df))
+df.to_csv(filename, index=False, header=True, encoding="utf-8-sig")
+print(filename + 'done')
