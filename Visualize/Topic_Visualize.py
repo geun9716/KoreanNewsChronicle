fc75e750f765469ae48dc91aa8459171e32a54d8
tkdxor97 <ghdtkdxor@naver.com>
2020-12-14 23:54:22 +0900

    visualize_year

--- Visualize/Topic_Visualize.py
+++ Visualize/Topic_Visualize.py
@@ -5,23 +5,32 @@ import pandas as pd
 import sys
 from collections import Counter
 
-df = pd.read_csv("./Visualize/201901_cluster_DBSCAN.csv", encoding="utf-8")
+df = pd.read_csv("./Visualize/2019.csv", encoding="utf-8")
 
 df.dropna()
-for i in range(0, 20, 1):
-    topics = []
-    for index, row in df.iterrows():
-        if(row['labels'] == i):
-            topics.extend(row['topics'].split(', '))
+for i in range(1, 13, 1):
+    for j in range(0, 100, 1):
+        count = 0
+        topics = []
+        for index, row in df.iterrows():
+            tmp = row['labels'].split(',')
+            month = tmp[0]
+            cluster_num = tmp[1]
+            if(int(month) == i and cluster_num == str(j)):
+                count+=1
+                topics.extend(row['topics'].split(', '))
+        if(count >= 40):
+            print(i, j)
+            hot_topic_index = Counter(topics).most_common(20)
+            hot_topic = dict(hot_topic_index)
 
-    hot_topic_index = Counter(topics).most_common(20)
-    hot_topic = dict(hot_topic_index)
+            print(hot_topic)
+            wordcloud = WordCloud(font_path='Visualize/malgun.ttf', background_color='white').generate_from_frequencies(dict(hot_topic))
 
-    print(hot_topic)
-    wordcloud = WordCloud(font_path='Visualize/malgun.ttf', background_color='white').generate_from_frequencies(dict(hot_topic))
-
-    plt.figure(figsize=(22,22)) #이미지 사이즈 지정
-    plt.imshow(wordcloud, interpolation='lanczos') #이미지의 부드럽기 정도
-    plt.axis('off') #x y 축 숫자 제거
-    # plt.show()
-    plt.savefig('cluster_' + str(i) + '.png')
+            plt.figure(figsize=(22,22)) #이미지 사이즈 지정
+            plt.imshow(wordcloud, interpolation='lanczos') #이미지의 부드럽기 정도
+            plt.axis('off') #x y 축 숫자 제거
+            # plt.show()
+            plt.savefig('month_' + str(i) + '_cluster_' + str(j) + '.png')
+        elif(count == 0):
+            break
