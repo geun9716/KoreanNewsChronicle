1d0eb896a313acdd1f08e6d382a24b2facc362e9
geun9716 <crintop@gmail.com>
2021-06-04 00:20:54 +0900

    0604_LDAtopic

--- Data/merge_file.py
+++ Data/merge_file.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 lists = []
 for i in range(1, 7):
-    path = "2019{0:02d}.csv".format(i)
+    path = "../Topic_Abstract/2021_new/2019{0:02d}_LDA_topic3.csv".format(i)
     lists.append(pd.read_csv(path, encoding="utf-8-sig"))
 
 df = pd.concat(lists)
-df.to_csv("./2019/2019.csv", index=False)
+df.to_csv("./2019/2019_LDA.csv", index=False)
